9905d67a148c285657ac0415c4f7dce8aceef027
Rimantas <belauzas@yahoo.com>
2022-06-29 10:08:01 +0300

    Comments for GIT conflict prevention

---
+++ js/pages/home.js
@@ -0,0 +1,29 @@
+/* header start */
+/* header end */
+
+/* hero start */
+/* hero end */
+
+/* about me start */
+/* about me end */
+
+/* services start */
+/* services end */
+
+/* portfolio start */
+/* portfolio end */
+
+/* testimonials start */
+/* testimonials end */
+
+/* achievements start */
+/* achievements end */
+
+/* our blog start */
+/* our blog end */
+
+/* contact us start */
+/* contact us end */
+
+/* footer start */
+/* footer end */
