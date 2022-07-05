a556c5a12f9e0b06d53ac98b192a22f155ee4a50
Rimantas <belauzas@yahoo.com>
2022-07-05 11:12:24 +0300

    Rendering portfolio gallery

--- js/components/gallery/Gallery.js
+++ js/components/gallery/Gallery.js
@@ -30,7 +30,6 @@ class Gallery {
     findTargetElement() {
         this.DOM = document.querySelector(this.selector);
         return !!this.DOM;
-        // return this.DOM ? true : false;
     }
 
     isValidData() {
@@ -41,8 +40,50 @@ class Gallery {
         return true;
     }
 
+    capitalize(text) {
+        return text[0].toUpperCase() + text.slice(1);
+    }
+
+    filterHTML() {
+        let HTML = '<li class="option active">All</li>';
+        const uniqueTags = [];
+
+        for (const { tags } of this.data) {
+            for (const tag of tags) {
+                if (uniqueTags.includes(tag)) {
+                    continue;
+                }
+                uniqueTags.push(tag);
+                HTML += `<li class="option">${this.capitalize(tag)}</li>`;
+            }
+        }
+
+        return HTML;
+    }
+
+    contentHTML() {
+        let HTML = '';
+
+        for (const item of this.data) {
+            HTML += `<div class="item">
+                        <img src="#" alt="Gallery image">
+                        <div class="overlay">
+                            <div class="title">Bulbs</div>
+                            <div class="actions">
+                                <a href="#" class="fa fa-chain-broken"></a>
+                                <a href="#" class="fa fa-search-plus"></a>
+                            </div>
+                        </div>
+                    </div>`;
+        }
+
+        return HTML;
+    }
+
     render() {
-        console.log('piesiam...');
+        this.DOM.innerHTML = `
+            <ul class="filter">${this.filterHTML()}</ul>
+            <div class="content">${this.contentHTML()}</div>`;
     }
 
     enableFilter() {
